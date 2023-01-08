56eaad2a7abe17eca68fc74398415ed187558edb
Diamantto <artur.grishko.2002@gmail.com>
2023-01-08 22:18:41 +0200

    Setup navigation

---
+++ app/src/main/java/com/example/mvvmnews/ui/NewsActivity.kt
@@ -0,0 +1,24 @@
+package com.example.mvvmnews.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.example.mvvmnews.R
+import com.example.mvvmnews.databinding.ActivityNewsBinding
+
+class NewsActivity : AppCompatActivity() {
+    lateinit var binding: ActivityNewsBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityNewsBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.newsNavHostFragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        binding.bottomNavigationView.setupWithNavController(navController)
+    }
+}
